c455c1f387ad5e07d0dc67f9b32af9c31063a26c
m.ruban <m.ruban@testportal.gov.ua>
2024-05-03 14:05:37 +0200

    add shift row and col

--- 15_puzzle/15_game.py
+++ 15_puzzle/15_game.py
@@ -10,10 +10,12 @@ COLOR_TEXT = (65, 105, 225)
 COLOR_BLOCK = (200, 200, 200)
 COLOR_FILL = (255, 255, 255)
 
+BORDER_SIZE = 4
+
 # Определение параметров окна
 WIDTH = 400
 HEIGHT = 400
-CELL_SIZE = WIDTH // 4
+CELL_SIZE = WIDTH // BORDER_SIZE
 
 # Радиус круглого угла фишки
 RADIUS = 20
@@ -38,14 +40,14 @@ def is_can_solved(lst):
 
 # Функция для создания и перемешивания пятнашек
 def create_board():
-    board = [[0] * 4 for _ in range(4)]
-    numbers = list(range(16))
+    board = [[0] * BORDER_SIZE for _ in range(BORDER_SIZE)]
+    numbers = list(range(BORDER_SIZE**2))
     random.shuffle(numbers)
     while not is_can_solved(numbers):
         random.shuffle(numbers)
     k = 0
-    for i in range(4):
-        for j in range(4):
+    for i in range(BORDER_SIZE):
+        for j in range(BORDER_SIZE):
             board[i][j] = numbers[k]
             k += 1
     return board
@@ -53,8 +55,8 @@ def create_board():
 
 # Функция для нахождения пустой ячейки
 def find_empty_cell(board):
-    for i in range(4):
-        for j in range(4):
+    for i in range(BORDER_SIZE):
+        for j in range(BORDER_SIZE):
             if board[i][j] == 0:
                 return i, j
 
@@ -62,9 +64,9 @@ def find_empty_cell(board):
 # Функция для проверки завершения игры
 def check_win(board):
     k = 1
-    for i in range(4):
-        for j in range(4):
-            if board[i][j] != k % 16:
+    for i in range(BORDER_SIZE):
+        for j in range(BORDER_SIZE):
+            if board[i][j] != k % BORDER_SIZE**2:
                 return False
             k += 1
     return True
@@ -73,6 +75,10 @@ def check_win(board):
 # Функция для обмена пятнашки с пустой ячейкой
 def swap(board, row1, col1, row2, col2):
     board[row1][col1], board[row2][col2] = board[row2][col2], board[row1][col1]
+
+
+def condition(a, b, mode):
+    return [a >= b, a <= b][mode]
 
 
 # Основная функция игры
@@ -95,11 +101,26 @@ def main():
                 if abs(row - empty_row) + abs(col - empty_col) == 1:
                     swap(board, row, col, empty_row, empty_col)
                     empty_row, empty_col = row, col
+                elif abs(row - empty_row) * abs(col - empty_col) == 0:
+                    if row == empty_row:
+                        mode = col > empty_col
+                        x, y = empty_row, empty_col + (-1) ** (mode + 1)
+                        while condition(y, col, mode):
+                            swap(board, x, y, empty_row, empty_col)
+                            empty_row, empty_col = x, y
+                            x, y = empty_row, empty_col + (-1) ** (mode + 1)
+                    elif col == empty_col:
+                        mode = row > empty_row
+                        x, y = empty_row + (-1) ** (mode + 1), empty_col
+                        while condition(x, row, mode):
+                            swap(board, x, y, empty_row, empty_col)
+                            empty_row, empty_col = x, y
+                            x, y = empty_row + (-1) ** (mode + 1), empty_col
 
         # Отрисовка игрового поля
         screen.fill(COLOR_FILL)
-        for i in range(4):
-            for j in range(4):
+        for i in range(BORDER_SIZE):
+            for j in range(BORDER_SIZE):
                 if board[i][j] != 0:
                     pygame.draw.rect(
                         screen,
@@ -120,7 +141,7 @@ def main():
 
         # Проверка завершения игры
         if check_win(board):
-            font = pygame.font.Font(None, 48)
+            font = pygame.font.Font(None, FONT_SIZE)
             text = font.render("Вы выиграли!", True, COLOR_BLOCK)
             text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
             screen.blit(text, text_rect)
